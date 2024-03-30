7f2278a9cd14a12eb821dea97c75c9c8dc878d8b
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-03-30 17:23:09 +0300

    Refactor search component to use city data

--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -1,21 +1,20 @@
 import { useState } from 'react'
 import { Combobox } from '@headlessui/react'
-
-const people = ['Durward Reynolds', 'Kenton Towne', 'Therese Wunsch', 'Benedict Kessler', 'Katelyn Rohan']
+import cityData from '../../data/city.json'
 
 const Search = () => {
-  const [selectedPerson, setSelectedPerson] = useState('')
+  const [selectedCity, setSelectedCity] = useState('')
   const [query, setQuery] = useState('')
 
-  const filteredPeople =
+  const filteredCity =
     query.length > 2
-      ? people.filter(person => {
-          return person.toLowerCase().includes(query.toLowerCase())
+      ? cityData.filter(city => {
+          return city.c_name.toLowerCase().includes(query.toLowerCase())
         })
-      : ['Please enter at least 3 characters']
+      : []
   return (
     <>
-      <Combobox as="div" className="search-container" value={selectedPerson} onChange={setSelectedPerson}>
+      <Combobox as="div" className="search-container" value={selectedCity} onChange={setSelectedCity}>
         <div className="city-input background-gray-600">
           <Combobox.Input
             className="text-md text-gray-400"
@@ -27,11 +26,19 @@ const Search = () => {
           </label>
         </div>
         <Combobox.Options className="search-item-container">
-          {filteredPeople.map(person => (
-            <Combobox.Option className="background-gray-500 text-gray-100 search-item text-md" key={person} value={person}>
-              {person}
-            </Combobox.Option>
-          ))}
+          {filteredCity.length > 0 ? (
+            filteredCity.map(city => (
+              <Combobox.Option as="Fragment" key={city.c_id} value={city.c_name}>
+                {({ active }) => (
+                  <li className={`background-gray-500 text-gray-100 search-item text-md ${active ? 'background-gray-600' : ''}`}>
+                    {city.c_name}
+                  </li>
+                )}
+              </Combobox.Option>
+            ))
+          ) : (
+            <li className="background-gray-500 text-gray-100 search-item text-md">No result found</li>
+          )}
         </Combobox.Options>
       </Combobox>
     </>
