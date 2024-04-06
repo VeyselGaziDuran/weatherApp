eada6b3fcbda8d6055a5933465e55081c4e8bb7c
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-04-06 22:34:07 +0300

    Update OpenWeatherMap API key and refactor API calls

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,10 +4,15 @@ import axios from 'axios'
 
 export const fetchWeatherData = async city => {
   try {
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&exclude=minutely,hourly&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&&lang=tr&units=metric`,
+    const cityData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`,
     )
-    return response.data
+    const coordinate = cityData.data.city.coord
+
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinate.lat}&lon=${coordinate.lon}&exclude=minutely,hourly&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&&lang=tr&units=metric`,
+    )
+    return { current: cityData.data, weekly: response.data }
   } catch (error) {
     console.log('Error fetching weather data', error)
   }
---
+++ src/utils/format-date.js
@@ -0,0 +1,23 @@
+const formatDate = dateString => {
+  const date = new Date(dateString)
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }
+  const formatter = new Intl.DateTimeFormat('en-US', options)
+  return formatter.format(date)
+}
+
+const removeTime = datetimeString => {
+  const parts = datetimeString.split(' ')
+  return parts[0]
+}
+
+const formatDateTime = datetimeString => {
+  const formattedDate = formatDate(removeTime(datetimeString))
+  return formattedDate
+}
+
+export default formatDateTime
