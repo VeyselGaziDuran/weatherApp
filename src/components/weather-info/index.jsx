5a586c1de6d66adcb5fabedafe957d370e620cc9
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-04-06 22:36:32 +0300

    Refactor WeatherInfo component and update CSS

--- src/components/weather-info/index.jsx
+++ src/components/weather-info/index.jsx
@@ -1,5 +1,6 @@
 import { useWeather } from '../../store/weather-store'
 import { useEffect } from 'react'
+import formatDateTime from '../../utils/format-date'
 
 const WeatherInfo = () => {
   const { weatherData } = useWeather()
@@ -7,11 +8,32 @@ const WeatherInfo = () => {
     console.log(weatherData)
   }, [weatherData])
   return (
-    <section>
-      <div className="background-gray-800 card-wrapper">sssss</div>
-      <div className="background-gray-800 card-wrapper"></div>
-      <div className="background-gray-800 card-wrapper"></div>
-    </section>
+    <>
+      {weatherData && (
+        <section className="background-gray-100 ">
+          <div className="background-gray-800 card-wrapper">
+            <div>
+              <span>
+                {weatherData?.current.city.name}, {weatherData?.current.city.country}
+              </span>
+              <span>{formatDateTime(weatherData?.current.list[0].dt_txt)}</span>
+            </div>
+            <div>
+              <span>{weatherData?.weekly.current.temp.toFixed(0)}°C</span>
+              <div>
+                <span>
+                  {weatherData?.weekly.daily[0].temp.min.toFixed(0)}°C / {weatherData?.weekly.daily[0].temp.max.toFixed(0)}°C{' '}
+                </span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+
+          <div className="background-gray-800 card-wrapper"></div>
+          <div className="background-gray-800 card-wrapper"></div>
+        </section>
+      )}
+    </>
   )
 }
 export default WeatherInfo
