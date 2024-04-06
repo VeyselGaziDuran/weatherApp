b708ea48756017b46b0061448e4b45c7f4bc95bc
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-04-07 01:33:31 +0300

    Refactor WeatherInfo component and update CSS

--- src/components/weather-info/index.jsx
+++ src/components/weather-info/index.jsx
@@ -14,21 +14,20 @@ const WeatherInfo = () => {
           <div className="background-gray-800 card-wrapper">
             <img src={`assets/bg-img/${weatherData.current.list[0].weather[0].icon}.png`} alt="bg-img" />
             <div>
-              <span>
+              <span className="text-gray-100 heading-sm">
                 {weatherData?.current.city.name}, {weatherData?.current.city.country}
               </span>
-              <span>{formatDateTime(weatherData.current.list[0].dt_txt)}</span>
+              <span className="text-gray-100 text-xs">{formatDateTime(weatherData.current.list[0].dt_txt)}</span>
             </div>
             <div>
-              <span>{weatherData?.weekly.current.temp.toFixed(0)}°C</span>
+              <span className="text-gray-100 heading-xl">{weatherData.weekly.current.temp.toFixed(0)}°C</span>
               <div>
-                <span>
-                  {weatherData?.weekly.daily[0].temp.min.toFixed(0)}°C / {weatherData?.weekly.daily[0].temp.max.toFixed(0)}°C{' '}
+                <span className="text-gray-100 heading-sm">
+                  {weatherData.weekly.daily[0].temp.min.toFixed(0)}°C / {weatherData?.weekly.daily[0].temp.max.toFixed(0)}°C{' '}
                 </span>
-                <span>
-                  <img src={`assets/weather-icons/${weatherData.current.list[0].weather[0].icon}.png`} alt="dynamic-img" />
-                </span>
+                <span className="text-gray-100 text-sm">{weatherData.weekly.current.weather[0].main}</span>
               </div>
+              <img src={`assets/weather-icons/${weatherData.current.list[0].weather[0].icon}.png`} alt="dynamic-img" />
             </div>
           </div>
 
