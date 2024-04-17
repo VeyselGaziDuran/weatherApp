813c2edefc04925e1d5b67fcfdc0ed2ff441812c
Veysel Gazi Duran <veyselduran82@gmail.com>
2024-04-17 20:39:11 +0300

    Update CSS in weather-info.css and icons.css, and refactor WeatherInfo component

--- src/components/weather-info/index.jsx
+++ src/components/weather-info/index.jsx
@@ -24,19 +24,19 @@ const WeatherInfo = () => {
             <img className="bg-img" src={`assets/bg-img/${weatherData.current.list[0].weather[0].icon}.png`} alt="bg-img" />
             <div className="overlay-content">
               <div className="city-data-wrapper">
-                <span className="text-gray-100 heading-sm">
+                <span className="text-gray-100 text-md text-bold">
                   {weatherData.current.city.name}, {weatherData.current.city.country}
                 </span>
                 <span className="text-gray-100 text-xs">{formatDateTime(weatherData.current.list[0].dt_txt)}</span>
               </div>
               <div className="city-​​weather-info">
                 <div className="city-​​weather-wrapper">
-                  <span className="text-gray-100 heading-xl">{weatherData.current.list[0].main.temp.toFixed(0)}°C</span>
+                  <span className="text-gray-100 heading-xl text-extrabold">{weatherData.current.list[0].main.temp.toFixed(0)}°C</span>
                   <div>
-                    <span className="text-gray-100 heading-sm">
+                    <span className="text-gray-100 heading-md text-bold">
                       {weatherData.weekly[0].min}°C / {weatherData.weekly[0].max}°C{' '}
                     </span>
-                    <span className="text-gray-100 text-sm">{weatherData.current.list[0].weather[0].main}</span>
+                    <span className="text-gray-100 text-sm text-bold">{weatherData.current.list[0].weather[0].main}</span>
                   </div>
                 </div>
                 <img
@@ -51,38 +51,38 @@ const WeatherInfo = () => {
           <div className="background-gray-800 card-wrapper">
             <div>
               <ThermometerIcon />
-              <span className="text-gray-200">Thermal sensation</span>
-              <span className="text-gray-100">{weatherData.current.list[0].main.feels_like.toFixed(0)}°C</span>
+              <span className="text-gray-200 text-bold heading-xs">Thermal sensation</span>
+              <span className="text-gray-100 text-bold heading-md">{weatherData.current.list[0].main.feels_like.toFixed(0)}°C</span>
             </div>
             <div>
               <CloudIcon />
-              <span className="text-gray-200">Probability of rain</span>
-              <span className="text-gray-100">{weatherData.current.list[0].pop.toFixed(0)}%</span>
+              <span className="text-gray-200 text-bold heading-xs">Probability of rain</span>
+              <span className="text-gray-100 text-bold heading-md">{weatherData.current.list[0].pop.toFixed(0)}%</span>
             </div>
             <div>
               <WindIcon />
-              <span className="text-gray-200">Wind speed</span>
-              <span className="text-gray-100">{weatherData.current.list[0].wind.speed.toFixed(0)} km/h</span>
+              <span className="text-gray-200 text-bold heading-xs">Wind speed</span>
+              <span className="text-gray-100 text-bold heading-md">{weatherData.current.list[0].wind.speed.toFixed(0)} km/h</span>
             </div>
             <div>
               <DropIcon />
-              <span className="text-gray-200">Air humidity</span>
-              <span className="text-gray-100">{weatherData.current.list[0].main.humidity.toFixed(0)}%</span>
+              <span className="text-gray-200 text-bold heading-xs">Air humidity</span>
+              <span className="text-gray-100 text-bold heading-md">{weatherData.current.list[0].main.humidity.toFixed(0)}%</span>
             </div>
             <div>
               <SunIcon />
-              <span className="text-gray-200">UV Index</span>
-              <span className="text-gray-100">5</span>
+              <span className="text-gray-200 text-bold heading-xs">UV Index</span>
+              <span className="text-gray-100 text-bold heading-md">5</span>
             </div>
           </div>
-          <div className="background-gray-800 card-wrapper">
+          <div className="background-gray-800 card-wrapper weekly-data">
             {weatherData.weekly.map((day, index) => (
-              <div key={index}>
-                <div>{getDay(day.date)}</div>
+              <div key={index} className="weekly-data-card">
+                <div className="text-gray-200 text-bold text-sm">{getDay(day.date)}</div>
                 <img src={`assets/weather-icons/${day.icon}.png`} alt="dynamic-img" />
                 <div>
-                  <span>{day.min}°C</span>
-                  <span>{day.max}°C</span>
+                  <span className="heading-sm text-gray-100 text-bold">{day.max}°C</span>
+                  <span className="heading-sm text-gray-400 text-bold">{day.min}°C</span>
                 </div>
               </div>
             ))}
